46eee18ca21b8f57b92ebd123d2ec0b78cd8b73a
Alejandro Zielinsky <azielinsky95@gmail.com>
2021-10-19 14:12:34 -0700

    Revert "test commit"
    This reverts commit 092533fbec3c40f6405127dd39f1c08cd847332a.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  //test
 } catch (error) {
   core.setFailed(error.message);
 }
